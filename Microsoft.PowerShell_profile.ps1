48ee24b1eedacd7fe790da1a8e8ae465062effda
Joey Shi <joeyshi12@gmail.com>
2024-01-08 19:33:34 -0800

    Added files

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+New-Alias -Name "v" nvim
+Set-PSReadlineOption -EditMode vi
+
+oh-my-posh init pwsh --config 'C:\Users\Joey Shi\AppData\Local\Programs\oh-my-posh\themes\powerlevel10k_lean.omp.json' | Invoke-Expression
